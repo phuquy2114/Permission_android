756c663132687a5b392c6f261eb52a8fae7793d4
phuquy <phuquycntt@gmail.com>
2020-03-13 20:46:25 +0700

    fix commit code

--- app/src/main/java/com/uits/permission/MainActivity.java
+++ app/src/main/java/com/uits/permission/MainActivity.java
@@ -10,6 +10,5 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
     }
 }
